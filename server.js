ea19586feae5ed2714d1bf01f96a9d45334438b2
Peter Bedor <peter.bedor@industry-arts.com>
2022-12-16 23:01:33 -0600

    tweaks

--- server.js
+++ server.js
@@ -8,23 +8,21 @@ const generateScreenshot = require('./lib/generateScreenshot.js');
 let context = null;
 
 async function init (browser) {
-  console.log('init');
   context = await browser.newContext();
 
   app.get('/generate', async (req, res) => {
     const { url } = req.query;
 
-    // console.time(`Screen shot generated for ${url} in`);
+    console.time(`Screen shot generated for ${url} in`);
 
     try {
-      console.log(url);
       res.setHeader('Content-Type', 'image/png');
       res.end(await generateScreenshot(context, url));
     } catch (e) {
       console.log(e);
       res.send(e);
     } finally {
-      // console.timeEnd(`Screen shot generated for ${url} in`);
+      console.timeEnd(`Screen shot generated for ${url} in`);
     }
   });
 
